042bbc40ac3481911e601da9f6f1fb81dd1b9327
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 14:53:41 -0800

    added: app.ts with Auth Controller

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import * as express from "express";
+import { Controller } from "./interfaces/controller.interface";
+import { errorMiddleware } from "./middleware/error.middleware";
+import { AuthController } from "./auth/auth.controller";
+
+export class App {
+  private app: express.Application;
+
+  constructor(controllers: Controller[]) {
+    this.app = express();
+
+    this.initializeMiddleware();
+    this.initializeControllers([new AuthController()]);
+    this.initializeErrorHandling();
+  }
+
+  public listen() {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`App Listening on Port ${process.env.PORT}`);
+    });
+  }
+
+  private initializeMiddleware() {
+    this.app.use(express.json());
+  }
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach(controller => {
+      this.app.use('/', controller.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+}
