b3f2aeb964fa5bd7e29c119a8f7902fd8b591271
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 10:01:53 -0800

    changed user.service to be more oop

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,8 @@
 import { query } from "../database";
+import { CreateUserDto } from "./user.dto";
 
-export const findUserbyId = async (id: number) => {
-  return await query("SELECT * FROM users WHERE id = $1", [id]);
-};
+export class UserService {
+  public findUserById = async (id: number) => await query("SELECT * FROM users where id = $1", [id]);
+  public findUserByEmail = async (email: string) => await query("SELECT * FROM users where email = $1", [email]);
+  public addUser = async (userData: CreateUserDto) => await query("INSERT INTO users(email, full_name, password_hash) VALUES($1, $2, $3) RETURNING *", [userData.email, userData.full_name, userData.password]);
+}
