ff6ddafd120ac9a7857fce6caedb1cb56949eb9a
Frederick Johnson <fritzj96@gmail.com>
2020-01-23 17:12:14 -0800

    added: auth.middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -4,15 +4,26 @@ import { AuthTokenMissingException } from "../exceptions/authTokenMissing.except
 import { WrongAuthTokenException } from "../exceptions/wrongAuthToken.exception";
 import { RequestWithUser } from "../interfaces/RequestWithUser.interface";
 import { DataStoredInToken } from "../interfaces/DataStoredInToken.interface";
+import { findUserbyId } from "../user/user.service";
 
-const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+export const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   const headers = req.headers;
   if (headers && headers.authorization) {
     const secret = process.env.JWT_SECRET;
     try {
-      const verificationResponse = jwt.verify(headers.authorization, secret) as
+      const verificationResponse = jwt.verify(headers.authorization, secret) as DataStoredInToken;
       const id = verificationResponse.id;
-
+      const user = findUserbyId(id);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        next(new WrongAuthTokenException());
+      }
+    } catch (error) {
+      next(new WrongAuthTokenException());
     }
+  } else {
+    next(new AuthTokenMissingException);
   }
 }
