623b9302de1d5de989095c4a8c62205e4b9b62b7
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 15:27:29 -0800

    added: query param typing

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,4 +2,4 @@ import { Pool } from "pg";
 
 const pool = new Pool();
 
-export const query = (text, params) => pool.query(text, params);
+export const query = (text: string, params: any[]) => pool.query(text, params);
