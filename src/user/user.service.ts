a4bae3ba9a26e3119e193e5b6a40ceb25df0c6b2
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 11:05:06 -0800

    added: whitespace to user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,9 @@ import { CreateUserDto } from "./user.dto";
 
 export class UserService {
   public findUserById = async (id: number) => await query("SELECT * FROM users where id = $1", [id]);
+
   public findUserByEmail = async (email: string) => await query("SELECT * FROM users where email = $1", [email]);
+
   public addUser = async (userData: CreateUserDto) => await query("INSERT INTO users(email, full_name, password_hash) VALUES($1, $2, $3) RETURNING *", [userData.email, userData.full_name, userData.password]);
+  
 }
