955f2dffdc3255d1d0fea2e9a936fcd4d5d9b89e
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 11:23:35 -0800

    changed: controller capitalization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { Controller } from "../interfaces/Controller.interface";
+import { Controller } from "../interfaces/controller.interface";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateUserDto } from "../user/user.dto";
 import { AuthService } from "./auth.service";
