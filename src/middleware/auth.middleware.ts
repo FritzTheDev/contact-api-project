143d08d53462ff541d8b6fb91484b5ba8c08f0fc
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 11:26:13 -0800

    added: authMiddleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,18 +2,19 @@ import { NextFunction, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import { AuthTokenMissingException } from "../exceptions/authTokenMissing.exception";
 import { WrongAuthTokenException } from "../exceptions/wrongAuthToken.exception";
-import { RequestWithUser } from "../interfaces/RequestWithUser.interface";
-import { DataStoredInToken } from "../interfaces/DataStoredInToken.interface";
-import { findUserbyId } from "../user/user.service";
+import { RequestWithUser } from "../interfaces/requestWithUser.interface";
+import { DataStoredInToken } from "../interfaces/dataStoredInToken.interface";
+import { UserService } from "../user/user.service";
 
 export const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const userService = new UserService();
   const headers = req.headers;
   if (headers && headers.authorization) {
     const secret = process.env.JWT_SECRET;
     try {
       const verificationResponse = jwt.verify(headers.authorization, secret) as DataStoredInToken;
       const id = verificationResponse.id;
-      const user = findUserbyId(id);
+      const user = userService.findUserById(id);
       if (user) {
         req.user = user;
         next();
