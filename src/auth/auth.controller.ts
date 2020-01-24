79e435277858b1601691a2add82bbf4e23fc63b8
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 11:16:17 -0800

    added: registration endpoint for auth control

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,4 +2,31 @@ import * as express from "express";
 import { Controller } from "../interfaces/Controller.interface";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateUserDto } from "../user/user.dto";
-import { AuthService } from "./auth"
+import { AuthService } from "./auth.service";
+
+export class AuthController implements Controller {
+  public path = "/auth";
+  public router = express.Router();
+  private authService = new AuthService();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDto), this.registration);
+  }
+
+  private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+    const userData: CreateUserDto = request.body;
+    try {
+      const {
+        token,
+        user
+      } = await this.authService.register(userData);
+      response.status(200).json({ token, user });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
