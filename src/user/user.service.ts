d1d0853fe72c39f0bc9879f270040c3231b7f03d
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 16:08:42 -0800

    removed: whitespace

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,5 @@ export class UserService {
 
   public findUserByEmail = async (email: string) => await query("SELECT * FROM users where email = $1", [email]);
 
-  public addUser = async (userData: CreateUserDto) => await query("INSERT INTO users(email, full_name, password_hash) VALUES($1, $2, $3) RETURNING *", [userData.email, userData.full_name, userData.password]);
-  
+  public addUser = async (userData: CreateUserDto) => await query("INSERT INTO users(email, full_name, password_hash) VALUES($1, $2, $3) RETURNING *", [userData.email, userData.full_name, userData.password]);  
 }
