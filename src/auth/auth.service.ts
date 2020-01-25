af1cdb96aa66ea8e3d86181ce27877e08e8dc641
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 15:45:44 -0800

    formatted: auth.service & fixed bug allowing any password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,45 +18,45 @@ export class AuthService {
       throw new UserWithThatEmailExistsException(userData.email);
     }
     userData.password = await bcrypt.hash(userData.password, 10);
-    const userArray = await this.userService.addUser(userData)
+    const userArray = await this.userService.addUser(userData);
     const user = userArray.rows[0];
     user.password_hash = undefined;
     const token = this.createToken({ id: user.id });
     return {
       token,
       user
-    }
+    };
   }
 
   public async login(loginData: LoginDto) {
-    const userQueryResult = await this.userService.findUserByEmail(loginData.email);
+    const userQueryResult = await this.userService.findUserByEmail(
+      loginData.email
+    );
     if (userQueryResult.rowCount === 0) {
       throw new UserDoesNotExistException(loginData.email);
     }
     const user = userQueryResult.rows[0];
-    try {
-      // Will throw on mismatch
-      await bcrypt.compare(loginData.password, user.password_hash);
+    if (await bcrypt.compare(loginData.password, user.password_hash)) {
       const token = this.createToken({ id: user.id });
       user.password_hash = undefined;
       return {
         token,
         user
-      }
-    } catch {
+      };
+    } else {
       throw new PasswordDoesNotMatchException();
     }
   }
 
   private createToken = (user: any): TokenData => {
-    const expiresIn = 60 * 60 * 24 * 7 // One Week
+    const expiresIn = 60 * 60 * 24 * 7; // One Week
     const secret = process.env.JWT_SECRET;
     const dataStoredinToken: DataStoredInToken = {
       id: user.id
-    }
+    };
     return {
       expiresIn,
       token: jwt.sign(dataStoredinToken, secret, { expiresIn })
-    }
-  }
+    };
+  };
 }
