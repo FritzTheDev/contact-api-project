34ae8f19500862eb14f5dfdd01f6f54939495c2f
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 21:05:51 -0800

    added: auth.controller login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { Controller } from "../interfaces/controller.interface";
 import { validationMiddleware } from "../middleware/validation.middleware";
 import { CreateUserDto } from "../user/user.dto";
+import { LoginDto } from "./login.dto";
 import { AuthService } from "./auth.service";
 
 export class AuthController implements Controller {
@@ -15,16 +16,24 @@ export class AuthController implements Controller {
 
   private initializeRoutes() {
     this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDto), this.registration);
+    this.router.post(`${this.path}/login`, validationMiddleware(LoginDto), this.login);
   }
 
   private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
     const userData: CreateUserDto = request.body;
     try {
-      const {
-        token,
-        user
-      } = await this.authService.register(userData);
-      response.status(200).json({ token, user });
+      const tokenAndUser = await this.authService.register(userData);
+      response.status(200).json(tokenAndUser);
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+  private login = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+    const loginData: LoginDto = request.body;
+    try {
+      const tokenAndUser = await this.authService.login(loginData);
+      response.status(200).json(tokenAndUser);
     } catch (error) {
       next(error);
     }
