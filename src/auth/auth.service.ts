330580e0d53770c82f3ebb2dcdfdf4ac1b161345
Frederick Johnson <fritzj96@gmail.com>
2020-01-24 20:59:32 -0800

    added: login function to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { UserWithThatEmailExistsException } from "../exceptions/userWithThatEmailExists.exception";
+import { PasswordDoesNotMatchException } from "../exceptions/passwordDoesNotMatch.exception";
+import { UserDoesNotExistException } from "../exceptions/userDoesNotExist.exception";
 import { DataStoredInToken } from "../interfaces/dataStoredInToken.interface";
 import { TokenData } from "../interfaces/tokenData.interface";
 import { CreateUserDto } from "../user/user.dto";
 import { UserService } from "../user/user.service";
+import { LoginDto } from "./login.dto";
 
 export class AuthService {
   private userService = new UserService();
@@ -16,12 +19,31 @@ export class AuthService {
     }
     userData.password = await bcrypt.hash(userData.password, 10);
     const userArray = await this.userService.addUser(userData)
-    const user = userArray.rows[0] as any;
-    user.password = undefined;
-    const token = this.createToken(user);
+    const user = userArray.rows[0];
+    user.password_hash = undefined;
+    const token = this.createToken({ id: user.id });
     return {
       token,
       user
+    }
+  }
+
+  public async login(loginData: LoginDto) {
+    const userQueryResult = await this.userService.findUserByEmail(loginData.email);
+    if (userQueryResult.rowCount === 0) {
+      throw new UserDoesNotExistException(loginData.email);
+    }
+    const user = userQueryResult.rows[0];
+    try {
+      // Will throw on mismatch
+      await bcrypt.compare(loginData.password, user.password_hash);
+      const token = this.createToken({ id: user.id });
+      return {
+        token,
+        user
+      }
+    } catch {
+      throw new PasswordDoesNotMatchException();
     }
   }
 
