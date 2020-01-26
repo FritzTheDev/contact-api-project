38cc56ba0c7ab54e19c5038789d89b60ce1b2889
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 11:28:25 -0800

    added: contact service comments

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -2,6 +2,13 @@ import { query } from "../database";
 import { ContactDto } from "./contact.dto";
 
 export class ContactService {
+  /*
+  * Handles SQL queries relating to contacts
+  * Basically just an interface between the db & the app;
+  * SQL queries happen here so they're less coupled.
+  */
+
+  // implicit return makes this neat.
   public findContactsByGroup = async (groupId: number) =>
     await query("SELECT * FROM contacts WHERE group_id = $1", [groupId]);
 
