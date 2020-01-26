0235c7ca2364fc126aa207985402a73577217d31
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 20:32:47 -0800

    added: findOwnedGroups

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -4,4 +4,6 @@ import { CreateGroupDto } from "./createGroup.dto";
 
 export class GroupService {
   public createGroup = async (groupData: CreateGroupDto, userId: number) => await query("INSERT INTO groups(group_name, user_id) VALUES($1, $2) RETURNING *", [groupData.group_name, userId]);
+
+  public findOwnedGroups = async (userId: number) => await query("SELECT * FROM groups where user_id = $1", [userId]);
 }
