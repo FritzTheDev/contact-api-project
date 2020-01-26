a511044543b909f6184608c8a02ab865bbeca531
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 19:51:04 -0800

    added: GroupController to server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 import { validateEnv } from "./utils/validateEnv";
 import { AuthController } from "./auth/auth.controller";
 import { App } from "./app";
+import { GroupController } from "./group/group.controller";
 
 validateEnv();
 
 const app = new App([
-  new AuthController()
+  new AuthController(),
+  new GroupController()
 ]);
 
 app.listen();
