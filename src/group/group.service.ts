84784491d53ee0db24424c69bab172368d9c1072
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 11:36:30 -0800

    added: comment to group service

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -3,6 +3,7 @@ import { CreateGroupDto } from "./createGroup.dto";
 
 
 export class GroupService {
+  // works as a layer between the controller & the database for the handling of the sql queries
   public createGroup = async (groupData: CreateGroupDto, userId: number) => await query("INSERT INTO groups(group_name, owner_id) VALUES($1, $2) RETURNING *", [groupData.group_name, userId]);
 
   public findOwnedGroups = async (userId: number) => await query("SELECT * FROM groups WHERE owner_id = $1", [userId]);
