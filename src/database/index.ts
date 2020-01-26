b75a9088ab969dc37550414f8004a74e56c13081
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 11:29:23 -0800

    added: database index comments

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,7 @@
 import { Pool } from "pg";
 
+// Creates DB connection pool
 const pool = new Pool();
 
+// Makes passing queries easy & standardized across the app.
 export const query = (text: string, params: any[]) => pool.query(text, params);
