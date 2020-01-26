c26c67615c0cd3d3e6fbc97e7ce6799a9b8ce28a
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 20:33:31 -0800

    fix: updated group.service to use new naming

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -3,7 +3,7 @@ import { CreateGroupDto } from "./createGroup.dto";
 
 
 export class GroupService {
-  public createGroup = async (groupData: CreateGroupDto, userId: number) => await query("INSERT INTO groups(group_name, user_id) VALUES($1, $2) RETURNING *", [groupData.group_name, userId]);
+  public createGroup = async (groupData: CreateGroupDto, userId: number) => await query("INSERT INTO groups(group_name, owner_id) VALUES($1, $2) RETURNING *", [groupData.group_name, userId]);
 
-  public findOwnedGroups = async (userId: number) => await query("SELECT * FROM groups where user_id = $1", [userId]);
+  public findOwnedGroups = async (userId: number) => await query("SELECT * FROM groups where owner_id = $1", [userId]);
 }
