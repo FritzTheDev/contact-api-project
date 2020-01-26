5b87558227911f438644cdc96460f48cbda6b2e2
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 21:37:00 -0800

    added: contactController to server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import { validateEnv } from "./utils/validateEnv";
 import { AuthController } from "./auth/auth.controller";
 import { App } from "./app";
 import { GroupController } from "./group/group.controller";
+import { ContactController } from "./contact/contact.controller";
 
 validateEnv();
 
 const app = new App([
   new AuthController(),
-  new GroupController()
+  new GroupController(),
+  new ContactController()
 ]);
 
 app.listen();
