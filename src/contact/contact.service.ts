fa17e71a1a94e00aa64e2dd8d2c3873b65641d0f
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 21:18:55 -0800

    added: createContact method to contact service

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,6 +1,13 @@
 import { query } from "../database";
+import { ContactDto } from "./contact.dto";
 
 export class ContactService {
   public findContactsByGroup = async (groupId: number) =>
     await query("SELECT * FROM contacts WHERE group_id = $1", [groupId]);
+
+  public createContact = async (contactData: ContactDto, ownerId: number) =>
+    await query(
+      "INSERT INTO contacts(email, owner_id, group_id) VALUES($1, $2, $3) RETURN *",
+      [contactData.email, ownerId, contactData.group_id]
+    );
 }
