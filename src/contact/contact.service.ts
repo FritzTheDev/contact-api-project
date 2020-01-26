67aebe47ef7f3aa4870989c136b818218b00b059
Frederick Johnson <fritzj96@gmail.com>
2020-01-25 20:53:42 -0800

    added: contactService

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -1,5 +1,6 @@
 import { query } from "../database";
 
 export class ContactService {
-  public findContactsByGroup = await query("SELECT * FROM contacts WHERE")
+  public findContactsByGroup = async (groupId: number) =>
+    await query("SELECT * FROM contacts WHERE group_id = $1", [groupId]);
 }
