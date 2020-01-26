d2a52b059edbe4ec64cf9783979f61c527597f5b
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 11:35:33 -0800

    added: groupDto comment

--- src/group/createGroup.dto.ts
+++ src/group/createGroup.dto.ts
@@ -1,6 +1,7 @@
 import { IsString, IsNumber, IsOptional } from "class-validator";
 
 export class CreateGroupDto {
+  // Used with validationMiddleware to validate data sent over the wire
   @IsString()
   group_name: string;
 }
