3d38a7fe9a04678429013de0e1a035b91ad01ca5
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 11:16:46 -0800

    removed: tokenData interface

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,29 +5,50 @@ import { PasswordDoesNotMatchException } from "../exceptions/passwordDoesNotMatc
 import { UserDoesNotExistException } from "../exceptions/userDoesNotExist.exception";
 import { EmailNotValidException } from "../exceptions/emailNotValid.exception";
 import { DataStoredInToken } from "../interfaces/dataStoredInToken.interface";
-import { TokenData } from "../interfaces/tokenData.interface";
 import { CreateUserDto } from "../user/user.dto";
 import { UserService } from "../user/user.service";
 import { LoginDto } from "./login.dto";
 
 export class AuthService {
+  /*
+   * Handles the Auth logic so the auth controller is only responsible for request handling.
+   * includes methods corresponding with registration & login functionality
+   */
+
+  // "Injecting" a service into another service seems less than ideal from Angular experience
+  // but it works here.
   private userService = new UserService();
 
   public async register(userData: CreateUserDto) {
+    // Checks for existing user emails that match the provided one.
+    // This ensures they remain unique
     const emailCheck = await this.userService.findUserByEmail(userData.email);
     if (emailCheck.rowCount !== 0) {
+      // Throws a custom error that bubbles up to the controller if needed
       throw new UserWithThatEmailExistsException(userData.email);
     }
-    // Does a very basic check on whether an email is valid-ish
+    // Does a very basic check on whether an email is valid-ish.
+    // Checks for <string>@<string>.<string>, so it's not very robust.
+    // There are better regexes for email but I didn't want to go
+    // overboard considering the spec.
     const re = /\S+@\S+\.\S+/;
-    if(!re.test(userData.email)) {
+    if (!re.test(userData.email)) {
+      // Throws if the regex does not match.
       throw new EmailNotValidException(userData.email);
     }
+    // Hashes the password, with 10 rounds of salt.
     userData.password = await bcrypt.hash(userData.password, 10);
-    const userArray = await this.userService.addUser(userData);
-    const user = userArray.rows[0];
+    //
+    // Calls the user service's addUser method to insert the new user /w hashed pw
+    const dbResponse = await this.userService.addUser(userData);
+    // pulls the user object from the db response.
+    const user = dbResponse.rows[0];
+    // Strips the password hash after inserting it into the db,
+    // but before returning it to be added to the response.
     user.password_hash = undefined;
-    const token = `Bearer ${this.createToken({ id: user.id }).token}`
+    // creates Bearer token with the user's id as the only jwt property
+    //
+    const token = `Bearer ${this.createToken(user.id)}`;
     return {
       token,
       user
@@ -43,7 +64,7 @@ export class AuthService {
     }
     const user = userQueryResult.rows[0];
     if (await bcrypt.compare(loginData.password, user.password_hash)) {
-      const token = `Bearer ${this.createToken({ id: user.id }).token}`;
+      const token = `Bearer ${this.createToken(user.id)}`;
       user.password_hash = undefined;
       return {
         token,
@@ -54,15 +75,12 @@ export class AuthService {
     }
   }
 
-  private createToken = (user: any): TokenData => {
+  private createToken = (id): string => {
     const expiresIn = 60 * 60 * 24 * 7; // One Week
     const secret = process.env.JWT_SECRET;
     const dataStoredinToken: DataStoredInToken = {
-      id: user.id
+      id
     };
-    return {
-      expiresIn,
-      token: jwt.sign(dataStoredinToken, secret, { expiresIn })
-    };
+    return jwt.sign(dataStoredinToken, secret, { expiresIn });
   };
 }
