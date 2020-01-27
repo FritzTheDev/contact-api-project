4d93c26400c4c84a9b52816df4e54417031ba95b
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:34:49 -0800

    added: server notes

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,15 @@ import { App } from "./app";
 import { GroupController } from "./group/group.controller";
 import { ContactController } from "./contact/contact.controller";
 
+//validates envs needed for the app
 validateEnv();
 
 const app = new App([
+  // Controller array gets passed into app.ts constructor
   new AuthController(),
   new GroupController(),
   new ContactController()
 ]);
 
+// doesn't need a # because it's from the custom app class
 app.listen();
