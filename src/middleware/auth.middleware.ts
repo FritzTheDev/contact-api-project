cdc4b431461705f82fec1057fa7a65e70614b1b1
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:12:24 -0800

    added: auth middleware comments

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -7,19 +7,28 @@ import { DataStoredInToken } from "../interfaces/dataStoredInToken.interface";
 import { UserService } from "../user/user.service";
 
 export const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  // Pulls the user service in so I can compare user creds
   const userService = new UserService();
+  // get the bearer token from the Authorization Header
   const headers = req.headers;
   if (headers && headers.authorization) {
     const secret = process.env.JWT_SECRET;
+    // Slices the "Bearer " off the token
     const token = headers.authorization.slice(7)
     try {
+      // verifies the token is valid
       const verificationResponse = jwt.verify(token, secret) as DataStoredInToken;
+      // peels the id off the valid jwt
       const id = verificationResponse.id;
-      const user = await userService.findUserById(Number(id));
-      if (user) {
-        req.user = user.rows[0];
+      // finds the user by their id
+      const dbResponse = await userService.findUserById(Number(id));
+      if (dbResponse.rows[0]) {
+        // attaches the user to the request object
+        req.user = dbResponse.rows[0];
+        // goes to the next function in the chain
         next();
       } else {
+        // Error handling from here on down
         next(new WrongAuthTokenException());
       }
     } catch (error) {
@@ -28,4 +37,4 @@ export const authMiddleware = async (req: RequestWithUser, res: Response, next: 
   } else {
     next(new AuthTokenMissingException());
   }
-}
+}Z
