f8ae1e2cb05293d1805f7f515c9f6f207c1ae362
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:36:40 -0800

    added: validateEnv comments

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,5 +1,7 @@
 import { cleanEnv, str, port } from "envalid";
 
+// adds validation for env vars that are always required.
+// runs on server start, called in server.ts
 export const validateEnv = () => {
   cleanEnv(process.env, {
     JWT_SECRET: str(),
