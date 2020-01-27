a857db077768a32a85c0abcb65a8e3dccb65dd87
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:18:31 -0800

    added: comments for validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -6,18 +6,22 @@ import { HttpException } from "../exceptions/http.exception";
 
 export function validationMiddleware<T>(
   type: any,
-  skipMissingProperties = false
+  skipMissingProperties = false // this makes it throw on a missing property
 ): express.RequestHandler {
-  //
   return (req, res, next) => {
+    // runs validation on the body with the provided dto
     validate(plainToClass(type, req.body), { skipMissingProperties }).then(
+      // handles errors, noting which constraints were violated
       (errors: ValidationError[]) => {
         if (errors.length > 0) {
+          // joins triggered constraints
           const message = errors
             .map((error: ValidationError) => Object.values(error.constraints))
             .join(", ");
+          // passes error if invalid
           next(new HttpException(400, message));
         } else {
+          // progresses if valid
           next();
         }
       }
