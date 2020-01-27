ab940393889abd33ae0133d34a8dd61d0aed1455
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:14:01 -0800

    added: error middleware comments

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -2,14 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/http.exception';
 
 export function errorMiddleware(
+  // handles any errors thrown by request handler chains
   error: HttpException,
   request: Request,
   response: Response,
   next: NextFunction
 ) {
+  // If the error hasn't been handled properly, it defaults to a 500 with a basic message
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
-  response.status(status).send({
+  response.status(status).json({
     message,
     status
   });
