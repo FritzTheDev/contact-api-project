968093a674e2c5cf532907cbe31a6c9be0571b4e
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:35:59 -0800

    added: app.ts comments

--- src/app.ts
+++ src/app.ts
@@ -7,29 +7,33 @@ export class App {
 
   constructor(controllers: Controller[]) {
     this.app = express();
-
+    // sets up handlers
     this.initializeMiddleware();
     this.initializeControllers(controllers);
     this.initializeErrorHandling();
   }
 
   public listen() {
+    // called to start the server
     this.app.listen(process.env.PORT, () => {
       console.log(`App Listening on Port ${process.env.PORT}`);
     });
   }
 
   private initializeMiddleware() {
+    // other middleware for logging etc could go here.
     this.app.use(express.json());
   }
 
   private initializeControllers(controllers: Controller[]) {
+    // each controller gets added here
     controllers.forEach(controller => {
       this.app.use('/', controller.router);
     });
   }
 
   private initializeErrorHandling() {
+    // error handling gets added last
     this.app.use(errorMiddleware);
   }
 }
