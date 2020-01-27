d9a5f3203cc48185b6b902079d19df69cd6afa0a
Frederick Johnson <fritzj96@gmail.com>
2020-01-26 16:12:34 -0800

    added: interface comments

--- src/interfaces/requestWithUser.interface.ts
+++ src/interfaces/requestWithUser.interface.ts
@@ -1,5 +1,6 @@
 import { Request } from 'express';
 
 export interface RequestWithUser extends Request {
+  // adds the user property to request type so I can use authMiddleware correctly
   user: any;
 }
